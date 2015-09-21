2535d441f2c1c320a7695b056bde181205e830ee
wusuopubupt <wdxbupt2009@gmail.com>
2015-09-21 20:41:28 +0800

    翻转字符串

--- string/str_reverse.c
+++ string/str_reverse.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 
+
 void str_reverse(char *s, int i, int j);
 
 int main(int argc, char **argv) {
     //while(fgets(a, 1000, stdin)) {
         char a[] = "hello world";
         int n = sizeof(a)/sizeof(a[0]) - 2; 
-        str_reverse(a, 0, n);
+        str_reverse(a, 0, n); // "dlrow olleh"
         int i = 0, j = 0;
         while(a[i] != '\0') {
+            // 找到空格处或者终点
             while(a[j] != ' ') {
                 j++;
                 if(a[j] == '\0') {
