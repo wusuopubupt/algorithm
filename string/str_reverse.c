cb56f9a0c5f6450668a3c963678c9cb9dc5ebbae
wusuopubupt <wdxbupt2009@gmail.com>
2015-09-21 20:34:22 +0800

    新文件：   str_reverse.c

---
+++ string/str_reverse.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void str_reverse(char *s, int i, int j);
+
+int main(int argc, char **argv) {
+    //while(fgets(a, 1000, stdin)) {
+        char a[] = "hello world";
+        int n = sizeof(a)/sizeof(a[0]) - 2; 
+        str_reverse(a, 0, n);
+        int i = 0, j = 0;
+        while(a[i] != '\0') {
+            while(a[j] != ' ') {
+                j++;
+                if(a[j] == '\0') {
+                    break;
+                }
+            }
+            str_reverse(a, i, j-1);
+            i = ++j;
+        }
+    //}
+    printf("%s", a);
+    return 0;
+}
+
+void str_reverse(char *a, int i, int j) {
+    //printf("func called! i:%d, j:%d\n", i, j);
+    while(i < j) {
+        char temp = a[j];
+        a[j] = a[i];
+        a[i] = temp;
+        i++;
+        j--;
+    }
+}
